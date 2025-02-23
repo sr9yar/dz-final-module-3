d232c3d80873e427fe9a0a7f1629bd4e43d36c13
Yaroslav <340999@gmail.com>
2025-02-23 21:55:50 +0300

    reports updated

--- programs.py
+++ programs.py
@@ -5,7 +5,7 @@ programs = [
     {"Program": "Adobe Reader", "Version": "18.009.20050"}, # version changed
     {"Program": "nginx", "Version": "1.14.0"},
     {"Program": "Apache HTTP Server", "Version": "2.4.29"},
-    {"Program": "DjVu Reader", "Version": "2.0.0.27"}, # missing
+    # {"Program": "DjVu Reader", "Version": "2.0.0.27"}, # missing
     {"Program": "Wireshark", "Version": "2.6.1"},
     {"Program": "Notepad++", "Version": "8.0"}, # version changed
     {"Program": "Google Chrome", "Version": "68.0.3440.106"}, 
